db376c553388abfd7b03576f994cf5f19fdecb8b
matthewh26 <115488877+matthewh26@users.noreply.github.com>
2023-11-02 23:16:41 +0000

    Agent nearly ready to go
    Just need to add get_action method and add functionality to update opponent's model regularly

--- agent.py
+++ agent.py
@@ -3,29 +3,32 @@ from game import board, player, QuoridorGame
 from collections import deque
 import numpy as np
 import model
+import random
 
 MAX_MEMORY = 10000000
-BATCH_SIZE = 100
+BATCH_SIZE = 1000
 LR = 0.001
 
 class agent():
-    def __init__(self):
+    def __init__(self, player):
         self.number_of_games = 0
         self.epsilon = 0 #randomness
         self.gamma = 0 #discount rate
         self.memory = deque(maxlen=MAX_MEMORY) #popleft()
-        self.model = model.Linear_QNet(289,140,500)
-        self.trainer = None #TO DO
+        self.model = model.Linear_QNet(290,140,500)
+        self.trainer = model.QTrainer(self.p1_model, self.gamma, LR)
+        self.player = player
 
-    def get_state(self,game):
-        state = np.concatenate(game.board.squares,game.board.walls_vert,game.board.walls_hor,game.board.vertices, axis=None)
+    def get_state(self, game):
+        state = np.concatenate(game.board.squares,game.board.walls_vert,game.board.walls_hor,game.board.vertices,self.player, axis=None)
         return state
     
-    def get_action(self, state, game):
+    def get_action(self, state, game, player):
         self.epsilon = 1000 - self.num_games
         final_move = np.zeros(140)
+            
         if np.random.randint(0,200) < self.epsilon:
-            move_nums = np.random.shuffle(np.arange(140)-1)
+            move_nums = np.random.shuffle(np.arange(140))
             while True:
                 i = 0
                 movetype, direction, locations = game.get_action_vars(move_nums[i])
@@ -41,18 +44,28 @@ class agent():
     def remember(self,state, action, reward, next_state, game_over):
         self.memory.append([state,action,reward,next_state,game_over])
 
+
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+        
+        states,actions,rewards,next_states,game_overs = zip(*mini_sample)
+        self.trainer.train_step(states,actions,rewards,next_states,game_overs)
 
 def train():
     env = board()
     p1 = player(1, 'blue_pawn.png')
     p2 = player(2, 'white_pawn.png')
     game = QuoridorGame(p1,p2,env)
-    agent_1 = agent()
-    agent_2 = agent()
+    agent_1 = agent(1)
+    agent_2 = agent(2)
 
     while True:
+        #if it is the first move, and the learning agent is p2, do a move 
+        if agent_1.player == 2 and game.turn == 0:
+            game.play_step(agent_2.get_action(agent_2.get_state(game)))
 
         #get old state
         state_old = agent_1.get_state(game)
@@ -61,29 +74,46 @@ def train():
         final_move = agent_1.get_action(state_old)
 
         #perform move and get interim state
-        turn_reward, game_over = game.play_step(final_move)
-        state_interim = agent.get_state(game)
+        game_over, p1_reward, p2_reward = game.play_step(final_move)
+        rewards = [p1_reward, p2_reward]
+        reward += rewards[agent_1.player - 1]
 
-        if game_over:
-            game.reset()
-            agent.train_long_memory()
-
-        #opponent agent plays move, get the final state for the 'turn'
-        opp_move = agent_2.get_action(state_interim)
-        opp_reward, game_over = game.play_step(opp_move)
         state_new = agent.get_state(game)
 
-        #calc total turn reward
-        reward = opp_reward + turn_reward
-
         #remember
-        agent.remember(state_old, final_move, reward, state_new, game_over)
+        agent_1.remember(state_old, final_move, reward, state_new, game_over)
 
         if game_over:
             game.reset()
-            agent.train_long_memory()
+            agent_1.train_long_memory()
 
-        return 0
+            agent_1.player = np.random.randint(1,3)
+            agent_2.player = (agent_1.player % 2) + 1
+            agent_1.number_of_games, agent_2.number_of_games += 1
+        
+        else:
+
+            #opponent agent plays move, get the final state for the 'turn'
+            opp_move = agent_2.get_action(state_new)
+            game_over, p1_reward, p2_reward= game.play_step(opp_move)
+
+
+            if game_over:
+                reward = -10
+                agent_1.memory.pop()
+                agent_1.remember(state_old, final_move, reward, state_new, game_over)
+                game.reset()
+                agent_1.train_long_memory()
+
+                agent_1.player = np.random.randint(1,3)
+                agent_2.player = (agent_1.player % 2) + 1
+                agent_1.number_of_games, agent_2.number_of_games += 1
+                
+
+        #reset reward
+        reward = 0
+
+
 
 #prediction_values = np.argsort
 #while True:
@@ -93,6 +123,9 @@ def train():
 #       break
 #   else:
 #       i += 1
+
+#also TO DO: Save state dicts, update the opponent's model every so often so
+#the opponent also improves during training
 
 if __name__ == '__main__':
     train()
